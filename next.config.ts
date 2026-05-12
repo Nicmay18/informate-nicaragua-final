78d8b71ecb19eb8ef1f7e2122c51aff33ab0c34e
Nicmay18 <maycolnicaragua2018@gmail.com>
2026-05-11 22:15:30 -0600

    fix: agregar route.js a eslintignore para arreglar build de Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
+    dirs: ['app', 'components', 'lib'],
   },
   typescript: {
     ignoreBuildErrors: false,
